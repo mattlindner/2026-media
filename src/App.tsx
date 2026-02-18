dd0f8a41ab7c814c8331849bfc3787a9de940508
mattlindner <matthew.d.lindner@gmail.com>
2026-02-18 17:55:11 -0600

    unused import

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from "react";
+import { useMemo, useState } from "react";
 import "./App.css";
 import media, { Media } from "./media";
 import Week from "./Week/Week";
